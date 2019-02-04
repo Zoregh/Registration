4555638d0a37be73afe8399438869df012c00f0e
Zoregh <zoreghhakobyan@gmail.com>
2019-02-04 13:07:23 +0400

    added JQ, BS, components and guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoggedGuard } from './Guards/logged.guard';
+import { LoginComponent } from './Components/login/login.component';
+import { RegistrationComponent } from './Components/registration/registration.component';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'home', component: HomeComponent, canActivate: [LoggedGuard]},
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
